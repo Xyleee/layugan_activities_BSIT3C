14a99808c5e87d0c3d423e91aec3bdc77d34c8c1
Xyleee <jameskesbacosa24@gmail.com>
2025-02-18 00:30:35 +0800

    Pusher

--- demoproj1/portfolio/views.py
+++ demoproj1/portfolio/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+data = [
+{"title": "Users", "count": 150},
+{"title": "Orders", "count": 320},
+{"title": "Revenue", "count": "12450"},
+]
 
 def index(request):
-    return render(request, "pages/portfolio.html")
+    return render(request, "pages/dashboard.html", {"data": data})
